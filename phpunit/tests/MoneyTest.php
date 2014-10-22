3cf5915da8ab1540f2606674e5241a188a6ed456
HienBV <hienbv.it@gmail.com>
2014-10-22 22:47:40 +0700

    Update code

--- phpunit/tests/MoneyTest.php
+++ phpunit/tests/MoneyTest.php
@@ -5,18 +5,24 @@
  *
  * @author hienbv
  */
-//include 'src/Money.php';
+include 'src/math.php';
 class MoneyTest extends PHPUnit_Framework_TestCase {
 
-    public function testCanBeNegated() {
-        // Arrange
-        $a = new Money(1);
-
-        // Act
-        $b = $a->negate();
-
-        // Assert
-        $this->assertEquals(-1, $b->getAmount());
+    public function testAdd() {
+        $this->assertEquals(4, add(2,2));
+        $this->assertEquals(-4, add(-2,-2));
+        $this->assertEquals(0, add(2,-2));
+        $this->assertEquals(0, add(-2,2));
+        $this->assertEquals(2, add(0,2));
+        $this->assertEquals(-2, add(0,-2));
     }
 
+    public function testSub() {
+        $this->assertEquals(0, sub(2,2));
+        $this->assertEquals(0, sub(-2,-2));
+        $this->assertEquals(4, sub(2,-2));
+        $this->assertEquals(-4, sub(-2,2));
+        $this->assertEquals(-2, sub(0,2));
+        $this->assertEquals(-2, sub(0,-2));
+    }
 }
